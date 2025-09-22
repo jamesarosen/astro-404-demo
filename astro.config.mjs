e372d57abdb7c30c3647ead90c1a19428059679e
James A Rosen <gh@confident.technology>
2025-09-22 09:26:22 -0700

    set edgeMiddleware: true

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,5 +5,7 @@ import netlify from '@astrojs/netlify';
 
 // https://astro.build/config
 export default defineConfig({
-  adapter: netlify()
+  adapter: netlify({
+    edgeMiddleware: true,
+  })
 });
