84de5b9291533a5e54d6fa978cf501e4ac1f4727
James A Rosen <gh@confident.technology>
2025-09-22 09:11:10 -0700

    add 404, middleware

---
+++ src/middleware/index.ts
@@ -0,0 +1,15 @@
+import { sequence } from 'astro:middleware'
+import type { MiddlewareHandler } from 'astro'
+
+export const onRequest: MiddlewareHandler = async (context, next) => {
+  const url = new URL(context.request.url)
+
+  if (url.pathname === '/health') {
+    const body = `OK at ${new Date()}\n`
+    return new Response(body, { headers: { 'content-length': body.length.toString() }})
+  }
+
+  const res = await next();
+  res.headers.set('astro-debug', `res.status=${res.status}`)
+  return res;
+}
